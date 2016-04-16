e16cd65bc51f653751aa9a897878e98b74d4c37f
Sayak Biswas <sayak90@gmail.com>
2016-04-15 21:17:44 -0400

    Changelog: 1. Added CardboardReticle growth feature. 2. Added ProcrastinationScript.cs

---
+++ Assets/Scripts/ProcrastinationScript.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProcrastinationScript : MonoBehaviour {
+	public CardboardReticle reticle;
+	public LayerMask layerMask;
+	private bool isPlayingGame = false;
+	public GameObject gameScreen;
+	public GameObject tv;
+	public GameObject gameScreen1;
+	public GameObject gameScreen2;
+	public GameObject gameScreen3;
+	public GameObject gameScreen4;
+	public Texture[] gameTextures;
+	private Vector3 screen1OriginalPosition;
+	private Vector3 screen2OriginalPosition;
+	private Vector3 screen3OriginalPosition;
+	private Vector3 screen4OriginalPosition;
+	private int screen1textureChangeCount = 0;
+	private int screen2textureChangeCount = 1;
+	private int screen3textureChangeCount = 2;
+	private int screen4textureChangeCount = 3;
+
+	// Use this for initialization
+	void Start () {
+		screen1OriginalPosition = gameScreen1.transform.localPosition;
+		screen2OriginalPosition = gameScreen2.transform.localPosition;
+		screen3OriginalPosition = gameScreen3.transform.localPosition;
+		screen4OriginalPosition = gameScreen4.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hitInfo;
+		if(Physics.Raycast (Cardboard.SDK.GetComponentInChildren<CardboardHead> ().Gaze, out hitInfo, Mathf.Infinity, layerMask)) {
+			GameObject hitObject = hitInfo.transform.gameObject;
+			if(hitObject.name.Contains ("TV") || hitObject.name.Contains ("controller") || hitObject.name.Contains ("laptop")) {
+				reticle.GetComponent<CardboardReticle> ().OnGazeStart (this.gameObject.GetComponentInChildren<Camera> (), 
+					hitObject, hitInfo.point);
+			} else {
+				reticle.GetComponent<CardboardReticle> ().OnGazeExit (this.gameObject.GetComponentInChildren <Camera> (), hitObject);
+			}
+		}
+
+		if(Input.GetButtonDown ("Fire1")) {
+			if (Physics.Raycast (Cardboard.SDK.GetComponentInChildren<CardboardHead> ().Gaze, out hitInfo, Mathf.Infinity, layerMask)) {
+				GameObject hitObject = hitInfo.transform.gameObject;
+				if(hitObject.name.Contains ("TV") || hitObject.name.Contains ("controller")) {
+					if(!isPlayingGame) {
+						StartGaming ();
+					} else {
+						StopGaming ();
+					}
+				}
+			}
+		}
+
+		if(isPlayingGame) {
+			gameScreen1.transform.Translate (0, 0, -Time.deltaTime * 1.4f);
+			if(gameScreen1.transform.localPosition.z <= -0.1f) {
+				screen1textureChangeCount += 1;
+				gameScreen1.GetComponent<Renderer> ().material.mainTexture = gameTextures[(screen1textureChangeCount % gameTextures.Length)];
+				gameScreen1.transform.localPosition = screen1OriginalPosition;
+			}
+
+			gameScreen2.transform.Translate (0, 0, -Time.deltaTime * 1.4f);
+			if(gameScreen2.transform.localPosition.z <= -0.1f) {
+				screen2textureChangeCount += 1;
+				gameScreen2.GetComponent<Renderer> ().material.mainTexture = gameTextures[(screen2textureChangeCount % gameTextures.Length)];
+				gameScreen2.transform.localPosition = screen2OriginalPosition;
+			}
+
+			gameScreen3.transform.Translate (0, Time.deltaTime * 1.4f, -Time.deltaTime * 1.4f);
+			if(gameScreen3.transform.localPosition.z <= -0.1f) {
+				screen3textureChangeCount += 1;
+				gameScreen3.GetComponent<Renderer> ().material.mainTexture = gameTextures[(screen3textureChangeCount % gameTextures.Length)];
+				gameScreen3.transform.localPosition = screen3OriginalPosition;
+			}
+
+			gameScreen4.transform.Translate (0, Time.deltaTime * 1.4f, -Time.deltaTime * 1.4f);
+			if(gameScreen4.transform.localPosition.z <= -0.1f) {
+				screen4textureChangeCount += 1;
+				gameScreen4.GetComponent<Renderer> ().material.mainTexture = gameTextures[(screen4textureChangeCount % gameTextures.Length)];
+				gameScreen4.transform.localPosition = screen4OriginalPosition;
+			}
+		}
+	}
+
+	private void StartGaming() {
+		gameScreen.SetActive (true);
+		CardboardAudioSource tvAudioSource = tv.GetComponent<CardboardAudioSource> ();
+		tvAudioSource.Play ();
+		gameScreen1.SetActive (true);
+		gameScreen2.SetActive (true);
+		gameScreen3.SetActive (true);
+		gameScreen4.SetActive (true);
+		isPlayingGame = true;
+	}
+
+	private void StopGaming() {
+		CardboardAudioSource tvAudioSource = tv.GetComponent<CardboardAudioSource> ();
+		tvAudioSource.Stop ();
+		gameScreen.SetActive (false);
+		gameScreen1.SetActive (false);
+		gameScreen2.SetActive (false);
+		gameScreen3.SetActive (false);
+		gameScreen4.SetActive (false);
+		isPlayingGame = false;
+	}
+}
