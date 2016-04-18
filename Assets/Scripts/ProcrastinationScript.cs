e2c060f8823834f93f12dc764beba6f74a652062
Sayak Biswas <sayak90@gmail.com>
2016-04-18 19:13:46 -0400

    Changelog: 1. Added animated social media images. 2. Screen darkens when procrastinating.

--- Assets/Scripts/ProcrastinationScript.cs
+++ Assets/Scripts/ProcrastinationScript.cs
@@ -20,6 +20,10 @@ public class ProcrastinationScript : MonoBehaviour {
 	private int screen2textureChangeCount = 1;
 	private int screen3textureChangeCount = 2;
 	private int screen4textureChangeCount = 3;
+	public GameObject[] procrastinationObjects;
+	public Light directionalLight;
+	public Light pointLight;
+	private bool isOnSocialMedia = false;
 
 	// Use this for initialization
 	void Start () {
@@ -28,7 +32,7 @@ public class ProcrastinationScript : MonoBehaviour {
 		screen3OriginalPosition = gameScreen3.transform.localPosition;
 		screen4OriginalPosition = gameScreen4.transform.localPosition;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		RaycastHit hitInfo;
@@ -52,7 +56,11 @@ public class ProcrastinationScript : MonoBehaviour {
 						StopGaming ();
 					}
 				} else if(hitObject.name.Contains ("laptop") || hitObject.name.Contains ("paper")) {
-					
+					if(!isOnSocialMedia) {
+						StartSocialMedia ();
+					} else {
+						StopSocialMedia ();
+					}
 				}
 			}
 		}
@@ -89,6 +97,10 @@ public class ProcrastinationScript : MonoBehaviour {
 	}
 
 	private void StartGaming() {
+		directionalLight.enabled = false;
+		RenderSettings.ambientIntensity = 0.0f;
+		DynamicGI.UpdateEnvironment ();
+		pointLight.enabled = true;
 		gameScreen.SetActive (true);
 		CardboardAudioSource tvAudioSource = tv.GetComponent<CardboardAudioSource> ();
 		tvAudioSource.Play ();
@@ -102,11 +114,42 @@ public class ProcrastinationScript : MonoBehaviour {
 	private void StopGaming() {
 		CardboardAudioSource tvAudioSource = tv.GetComponent<CardboardAudioSource> ();
 		tvAudioSource.Stop ();
+		pointLight.enabled = false;
+		directionalLight.enabled = true;
+		RenderSettings.ambientIntensity = 1.0f;
+		DynamicGI.UpdateEnvironment ();
 		gameScreen.SetActive (false);
 		gameScreen1.SetActive (false);
 		gameScreen2.SetActive (false);
 		gameScreen3.SetActive (false);
 		gameScreen4.SetActive (false);
 		isPlayingGame = false;
+	}
+
+	private void StartSocialMedia() {
+		directionalLight.enabled = false;
+		RenderSettings.ambientIntensity = 0.0f;
+		DynamicGI.UpdateEnvironment ();
+		pointLight.enabled = true;
+		float i = 0.0f;
+		foreach(GameObject gameObject in procrastinationObjects) {
+			i++;
+			gameObject.SetActive (true);
+			iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath (gameObject.name + "Path"), "time", 3.0f+i, 
+				"looptype", iTween.LoopType.loop, "orientToPath", true, "easetype", iTween.EaseType.linear, "islocal", false));
+		}
+		isOnSocialMedia = true;
+	}
+
+	private void StopSocialMedia() {
+		foreach(GameObject gameObject in procrastinationObjects){
+			iTween.Stop (gameObject);
+			gameObject.SetActive (false);
+		}
+		pointLight.enabled = false;
+		directionalLight.enabled = true;
+		RenderSettings.ambientIntensity = 1.0f;
+		DynamicGI.UpdateEnvironment ();
+		isOnSocialMedia = false;
 	}
 }
