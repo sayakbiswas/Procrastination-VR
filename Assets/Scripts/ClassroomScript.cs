51426d822564cd78cc8098603c1f721048c78d8e
Sayak Biswas <sayak90@gmail.com>
2016-04-19 18:41:16 -0400

    Changelog: 1. Added classroom ambience noise. 2. Added ClassroomScript.cs.

---
+++ Assets/Scripts/ClassroomScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using RogoDigital.Lipsync;
+
+public class ClassroomScript : MonoBehaviour {
+	private float waitBeforeProfessorStarts = 2.0f;
+	private bool hasProfStartedSpeaking = false;
+	public GameObject theProfessor;
+	public LipSyncData profLipSyncData;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(waitBeforeProfessorStarts > 0.0f) {
+			waitBeforeProfessorStarts -= Time.deltaTime;
+		} else {
+			if(!hasProfStartedSpeaking) {
+				theProfessor.GetComponent<LipSync> ().Play (profLipSyncData);
+				hasProfStartedSpeaking = true;
+			}
+		}
+	}
+}
