a53b2ed7d4ff9fd27295fdd00528329d58739e10
Izhar Shaikh <Izhar Shaikh>
2016-04-21 18:39:46 -0400

    Added the character rotation script

---
+++ Assets/Scripts/CharacterRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterRotation : MonoBehaviour {
+
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lookDir = target.position - transform.position;
+        lookDir.y = 0; // keep only the horizontal direction
+        transform.rotation = Quaternion.LookRotation(lookDir);
+    }
+
+
+
+}
